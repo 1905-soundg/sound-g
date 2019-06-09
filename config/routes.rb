d256867cdd60dcf1162a5d774076e0298a767855
sugiyamachihiro <cccabdy1012@gmail.com>
2019-06-09 04:23:48 +0000

    controller update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'products#index'
+
   devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
@@ -10,4 +13,18 @@ devise_for :users, controllers: {
   registrations: 'users/registrations'
 }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :admins
+  resources :users
+  resources :addresses
+  resources :carts
+  resources :orders
+  resources :order_details
+  resources :products
+  resources :discs
+  resources :musics
+  resources :artists
+  resources :favorites
+  resources :reviews
+  resources :labels
+  resources :genres
 end

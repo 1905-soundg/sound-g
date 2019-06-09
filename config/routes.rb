d90265720cbed203c370ad9d77488579f9f7705e
Ryusuke Kimura <49398619+acenazuna@users.noreply.github.com>
2019-06-09 13:35:19 +0900

    Revert "Sugi"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-
-  root 'products#index'
-
   devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
@@ -13,18 +10,4 @@ devise_for :users, controllers: {
   registrations: 'users/registrations'
 }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :admins
-  resources :users
-  resources :addresses
-  resources :carts
-  resources :orders
-  resources :order_details
-  resources :products
-  resources :discs
-  resources :musics
-  resources :artists
-  resources :favorites
-  resources :reviews
-  resources :labels
-  resources :genres
 end

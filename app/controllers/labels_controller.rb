cc3691045790d2235f3f3874981bc47fd8b33789
sugiyamachihiro <cccabdy1012@gmail.com>
2019-06-10 04:11:43 +0000

    artist/genre/label update

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,6 +1,15 @@
 class LabelsController < ApplicationController
 
 	def create
+		@label = Label.new
+		@label.save(label_params)
+		redirect_to ('/')
 	end
+
+	private
+
+    def label_params
+        params.require(:label).permit(:label)
+    end
 
 end

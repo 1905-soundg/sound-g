7431c399d2bc544f6f5ab87eaf96d1b303cd7951
sugiyamachihiro <cccabdy1012@gmail.com>
2019-06-10 07:51:52 +0000

    admin/artists

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,2 +1,15 @@
 class Admins::GenresController < ApplicationController
+
+def create
+		@genre = Genre.new
+		@genre.save(genre_params)
+		redirect_to ('/')
+	end
+
+	private
+
+    def genre_params
+        params.require(:genre).permit(:genre)
+    end
+
 end

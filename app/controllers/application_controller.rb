66e38a3ef06689d8b228432891e2c26f9818961d
Ryusuke Kimura <acenazuna@gmail.com>
2019-06-11 14:17:06 +0900

    機能一覧追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #管理者ログイン後機能一覧に遷移
+	def after_sign_in_path_for(resource)
+		  admin_path(resource)
+    end
 	 protected
 	  def configure_permitted_parameters
 	    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_k, :first_name_k, :postalcode, :address, :telephone_number])

69aa0a9b991c3226a4171ad88af3af6cefe5e1c7
fujitaryugo <ryu5.1m3s1k@gmil.com>
2019-06-12 08:52:57 +0000

    applicationcontroller update

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,29 @@
 class CartsController < ApplicationController
+	before_action :authenticate_user!
+	def index
+	 @carts = Cart.all
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	def create
+	 @cart_datail = Cart.new(cart_params)
+	 @cart.user_id = curret_user.id
+	 if @cart.save
+	 redrect_to('/')
+	 else
+	 render ('/users/sign_in')
+	 end
+    end
+
+    private
+
+    def cart_params
+    	params.require(:cart).permit(:quantity)
+    end
+
 end

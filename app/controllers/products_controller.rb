45c73978311aa76dc3856d796d136c5b9cc672ab
志村隼斗 <daken@shimurahayatonoMacBook-Air.local>
2019-06-12 16:15:51 +0900

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,20 @@
 class ProductsController < ApplicationController
 
+	def show
+		@Product = Product.find(params[:id])
+	end
+
 	def index
 		@products = Product.all
+	end
+
+	protected
+
+	def product_params
+		params.require(:product).permit(:album, :genre_id, :label_id, :image, :price, :stock_quantity, :sales_status,
+			                            discs_attributes: [:id, :disc_number, :product_id, :_destroy,
+			                            musics_attributes: [:id, :title, :track_number, :disc_id, :artist_id, :_destroy,
+		                                 ]])
 	end
 
 end

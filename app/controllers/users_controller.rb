7ab142f6b4b066d0f76eeb979014d2d5231f0365
Ryusuke Kimura <acenazuna@gmail.com>
2019-06-13 19:27:50 +0900

    サインアップ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,24 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		@address = Address.new
-		@addresses = Address.all
+		@user = User.new
+		@users = User.all
 	end
 
 	def update
+		@user = User.find(params[:id])
+		@user.update(user_params)
+		redirect_to user_path(@users.user_id)
 	end
 
 	def destroy
 	end
+end
+
+private
+
+ def user_params
+     params.require(:user).permit(:last_name, :first_name, :last_name_k, :first_name_k, :postalcode, :address, :telephone_number, :email, :password)
+ end
+
 end

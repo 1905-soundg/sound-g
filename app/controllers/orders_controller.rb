cc7d0ef22098f598c70782a5682ddad7a6f493ac
fujitaryugo <ryu5.1m3s1k@gmil.com>
2019-06-13 05:12:28 +0000

    params update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,10 @@ class OrdersController < ApplicationController
 	def success
 	end
 
+	private
 
-
+	def order_params
+		params.require(:order).permit(:user_id, :subtotal_price, :fee, :postalcode, :address, :last_name, :first_name, :order_status)
+	end
 
 end

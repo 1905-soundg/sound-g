796a4d2d352ad552c4ed4413e78254a4d2203d68
sugiyamachihiro <cccabdy1012@gmail.com>
2019-06-16 10:28:16 +0000

    view update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_054507) do
+ActiveRecord::Schema.define(version: 2019_06_16_075217) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -130,12 +130,6 @@ ActiveRecord::Schema.define(version: 2019_06_11_054507) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "last_name"
@@ -147,6 +141,8 @@ ActiveRecord::Schema.define(version: 2019_06_11_054507) do
     t.string "telephone_number"
     t.integer "user_status"
     t.integer "address_id"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
